90f6c26990fa50512d0661119795d3b393846ca3
Rohitilak <rohittilak086@gmail.com>
2024-10-18 14:23:06 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import WeatherApp from './components/WeatherApp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className="App">
-        <h3>Rohit Tilak Weather App</h3>
-     </div>
-     <WeatherApp/>
+      <div className='weather-containter'>
+        <div className='app'>
+          <h3>Sanjay Weather App</h3>
+        </div>
+        <div>
+          <WeatherApp />
+        </div>
+      </div>
     </>
   )
 }
